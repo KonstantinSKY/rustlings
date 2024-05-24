62eef4766eca63c4e8ae4ea764f042cea943f034
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/doc-comment-faeafa426cfbe238/build_script_build-faeafa426cfbe238.d - Added :

---
+++ target/debug/build/doc-comment-faeafa426cfbe238/build_script_build-faeafa426cfbe238.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/doc-comment-faeafa426cfbe238/build_script_build-faeafa426cfbe238: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/doc-comment-0.3.3/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/doc-comment-faeafa426cfbe238/build_script_build-faeafa426cfbe238.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/doc-comment-0.3.3/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/doc-comment-0.3.3/build.rs:
