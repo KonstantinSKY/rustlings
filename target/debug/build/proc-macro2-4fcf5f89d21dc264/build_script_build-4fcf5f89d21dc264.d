19af71372cb87b070447fded316b54a6711dc366
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/proc-macro2-4fcf5f89d21dc264/build_script_build-4fcf5f89d21dc264.d - Added :

---
+++ target/debug/build/proc-macro2-4fcf5f89d21dc264/build_script_build-4fcf5f89d21dc264.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/proc-macro2-4fcf5f89d21dc264/build_script_build-4fcf5f89d21dc264: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.81/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/proc-macro2-4fcf5f89d21dc264/build_script_build-4fcf5f89d21dc264.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.81/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.81/build.rs:
