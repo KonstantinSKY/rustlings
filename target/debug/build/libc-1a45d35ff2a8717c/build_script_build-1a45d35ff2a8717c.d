19d6d518f87d48e6f0c34f4220fcc1d8d17d755f
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/libc-1a45d35ff2a8717c/build_script_build-1a45d35ff2a8717c.d - Added :

---
+++ target/debug/build/libc-1a45d35ff2a8717c/build_script_build-1a45d35ff2a8717c.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/libc-1a45d35ff2a8717c/build_script_build-1a45d35ff2a8717c: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/libc-1a45d35ff2a8717c/build_script_build-1a45d35ff2a8717c.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.153/build.rs:
