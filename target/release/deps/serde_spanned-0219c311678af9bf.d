d6885017be0a01bd9f605aa50faa2f6e098c62cd
SKY <sky012877@gmail.com>
2024-05-24 12:52:21 -0700

    target/release/deps/serde_spanned-0219c311678af9bf.d - Added :

---
+++ target/release/deps/serde_spanned-0219c311678af9bf.d
@@ -0,0 +1,8 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/libserde_spanned-0219c311678af9bf.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/spanned.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/libserde_spanned-0219c311678af9bf.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/spanned.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/serde_spanned-0219c311678af9bf.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/spanned.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.6/src/spanned.rs:
