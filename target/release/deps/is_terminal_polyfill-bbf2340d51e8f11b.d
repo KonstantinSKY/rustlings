9fd5290a522068fe9eb0a4dda08aeb281d4ff909
SKY <sky012877@gmail.com>
2024-05-24 12:52:19 -0700

    target/release/deps/is_terminal_polyfill-bbf2340d51e8f11b.d - Added :

---
+++ target/release/deps/is_terminal_polyfill-bbf2340d51e8f11b.d
@@ -0,0 +1,7 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/libis_terminal_polyfill-bbf2340d51e8f11b.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.0/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/libis_terminal_polyfill-bbf2340d51e8f11b.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.0/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/is_terminal_polyfill-bbf2340d51e8f11b.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.0/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/is_terminal_polyfill-1.70.0/src/lib.rs:
