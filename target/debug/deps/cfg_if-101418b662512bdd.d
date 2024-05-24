e5197bb87ba8b87efd9135c542c14647aa37d84e
SKY <sky012877@gmail.com>
2024-05-24 13:02:04 -0700

    target/debug/deps/cfg_if-101418b662512bdd.d - Added :

---
+++ target/debug/deps/cfg_if-101418b662512bdd.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/deps/libcfg_if-101418b662512bdd.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/cfg_if-101418b662512bdd.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
