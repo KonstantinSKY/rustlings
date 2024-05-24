2e48a50bf53a015aef2e8fff96d223126c72acb1
SKY <sky012877@gmail.com>
2024-05-24 13:02:05 -0700

    target/debug/deps/predicates_tree-3259b20af7b6616d.d - Added :

---
+++ target/debug/deps/predicates_tree-3259b20af7b6616d.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/deps/libpredicates_tree-3259b20af7b6616d.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/predicates-tree-1.0.9/src/lib.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/predicates_tree-3259b20af7b6616d.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/predicates-tree-1.0.9/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/predicates-tree-1.0.9/src/lib.rs:
