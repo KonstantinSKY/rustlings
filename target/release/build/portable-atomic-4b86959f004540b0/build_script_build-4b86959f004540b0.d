79aaa881263c3c0a62f4453d471ec2531d3f9c29
SKY <sky012877@gmail.com>
2024-05-24 12:52:18 -0700

    target/release/build/portable-atomic-4b86959f004540b0/build_script_build-4b86959f004540b0.d - Added :

---
+++ target/release/build/portable-atomic-4b86959f004540b0/build_script_build-4b86959f004540b0.d
@@ -0,0 +1,9 @@
+/home/sky/Work/Projects/rustlings/target/release/build/portable-atomic-4b86959f004540b0/build_script_build-4b86959f004540b0: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/build.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/version.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/no_atomic.rs
+
+/home/sky/Work/Projects/rustlings/target/release/build/portable-atomic-4b86959f004540b0/build_script_build-4b86959f004540b0.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/build.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/version.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/no_atomic.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/build.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/version.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/portable-atomic-1.6.0/no_atomic.rs:
+
+# env-dep:CARGO_PKG_NAME=portable-atomic
