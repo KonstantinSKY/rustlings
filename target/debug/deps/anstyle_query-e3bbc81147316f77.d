4763e6f945fb9f047a53071f85d9087ca7fa14ea
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/deps/anstyle_query-e3bbc81147316f77.d - Added :

---
+++ target/debug/deps/anstyle_query-e3bbc81147316f77.d
@@ -0,0 +1,6 @@
+/home/sky/Projects/rustlings/target/debug/deps/libanstyle_query-e3bbc81147316f77.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/windows.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/anstyle_query-e3bbc81147316f77.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/windows.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.2/src/windows.rs:
