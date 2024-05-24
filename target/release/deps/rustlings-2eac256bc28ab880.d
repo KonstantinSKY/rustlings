49d05b9b933ec9d205e3bc7a4a5af64c43f2b2fe
SKY <sky012877@gmail.com>
2024-05-24 12:52:21 -0700

    target/release/deps/rustlings-2eac256bc28ab880.d - Added :

---
+++ target/release/deps/rustlings-2eac256bc28ab880.d
@@ -0,0 +1,10 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/rustlings-2eac256bc28ab880: src/main.rs src/ui.rs src/exercise.rs src/project.rs src/run.rs src/verify.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/rustlings-2eac256bc28ab880.d: src/main.rs src/ui.rs src/exercise.rs src/project.rs src/run.rs src/verify.rs
+
+src/main.rs:
+src/ui.rs:
+src/exercise.rs:
+src/project.rs:
+src/run.rs:
+src/verify.rs:
