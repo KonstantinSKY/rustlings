439162201f2e5c0bf9269d5b6d6c8c3c85a5d63f
SKY <sky012877@gmail.com>
2024-05-24 13:02:04 -0700

    target/debug/deps/inotify_sys-e9f5af18a9036f43.d - Added :

---
+++ target/debug/deps/inotify_sys-e9f5af18a9036f43.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/deps/libinotify_sys-e9f5af18a9036f43.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/inotify_sys-e9f5af18a9036f43.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs:
