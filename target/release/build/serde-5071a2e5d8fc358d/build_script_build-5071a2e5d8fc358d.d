b52131413c179b84dcbf5092350142f0d08438b1
SKY <sky012877@gmail.com>
2024-05-24 12:52:19 -0700

    target/release/build/serde-5071a2e5d8fc358d/build_script_build-5071a2e5d8fc358d.d - Added :

---
+++ target/release/build/serde-5071a2e5d8fc358d/build_script_build-5071a2e5d8fc358d.d
@@ -0,0 +1,5 @@
+/home/sky/Work/Projects/rustlings/target/release/build/serde-5071a2e5d8fc358d/build_script_build-5071a2e5d8fc358d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.202/build.rs
+
+/home/sky/Work/Projects/rustlings/target/release/build/serde-5071a2e5d8fc358d/build_script_build-5071a2e5d8fc358d.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.202/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.202/build.rs:
