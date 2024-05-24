fbfe0e8ccb5e9fdd10e016c230ba5889f76c266a
SKY <sky012877@gmail.com>
2024-05-24 13:02:05 -0700

    target/debug/deps/termtree-591aa2de6b660bd2.d - Added :

---
+++ target/debug/deps/termtree-591aa2de6b660bd2.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/deps/libtermtree-591aa2de6b660bd2.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termtree-0.4.1/src/lib.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/termtree-591aa2de6b660bd2.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termtree-0.4.1/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/termtree-0.4.1/src/lib.rs:
