e512bc823420a035260dedb9afc16a17a57e628d
SKY <sky012877@gmail.com>
2024-05-24 13:02:05 -0700

    target/debug/deps/serde_spanned-edec733331177fbb.d - Added :

---
+++ target/debug/deps/serde_spanned-edec733331177fbb.d
@@ -0,0 +1,6 @@
+/home/sky/Projects/rustlings/target/debug/deps/libserde_spanned-edec733331177fbb.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/spanned.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/serde_spanned-edec733331177fbb.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/spanned.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_spanned-0.6.5/src/spanned.rs:
