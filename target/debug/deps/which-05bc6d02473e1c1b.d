b2612761825fbe00d42cb2808af1f8b3fa32afc5
SKY <sky012877@gmail.com>
2024-05-24 13:02:05 -0700

    target/debug/deps/which-05bc6d02473e1c1b.d - Added :

---
+++ target/debug/deps/which-05bc6d02473e1c1b.d
@@ -0,0 +1,8 @@
+/home/sky/Projects/rustlings/target/debug/deps/libwhich-05bc6d02473e1c1b.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/checker.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/error.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/finder.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/which-05bc6d02473e1c1b.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/checker.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/error.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/finder.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/checker.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/error.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/which-6.0.1/src/finder.rs:
