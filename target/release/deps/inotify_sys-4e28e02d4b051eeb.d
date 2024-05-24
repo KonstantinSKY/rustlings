cd2711371aeb8230ff25ea7aa27865a53c809156
SKY <sky012877@gmail.com>
2024-05-24 12:52:19 -0700

    target/release/deps/inotify_sys-4e28e02d4b051eeb.d - Added :

---
+++ target/release/deps/inotify_sys-4e28e02d4b051eeb.d
@@ -0,0 +1,7 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/libinotify_sys-4e28e02d4b051eeb.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/libinotify_sys-4e28e02d4b051eeb.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/inotify_sys-4e28e02d4b051eeb.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-sys-0.1.5/src/lib.rs:
