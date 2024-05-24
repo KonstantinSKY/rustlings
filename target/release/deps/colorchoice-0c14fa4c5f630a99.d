83fedb6b058343d684d37241e830ff276de9e75d
SKY <sky012877@gmail.com>
2024-05-24 12:52:19 -0700

    target/release/deps/colorchoice-0c14fa4c5f630a99.d - Added :

---
+++ target/release/deps/colorchoice-0c14fa4c5f630a99.d
@@ -0,0 +1,7 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/libcolorchoice-0c14fa4c5f630a99.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/colorchoice-1.0.1/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/libcolorchoice-0c14fa4c5f630a99.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/colorchoice-1.0.1/src/lib.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/colorchoice-0c14fa4c5f630a99.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/colorchoice-1.0.1/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/colorchoice-1.0.1/src/lib.rs:
