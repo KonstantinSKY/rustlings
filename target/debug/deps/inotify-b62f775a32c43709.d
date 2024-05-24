f5088f3b8f8bf3422a00a7ece76c063910ec8b30
SKY <sky012877@gmail.com>
2024-05-24 13:02:04 -0700

    target/debug/deps/inotify-b62f775a32c43709.d - Added :

---
+++ target/debug/deps/inotify-b62f775a32c43709.d
@@ -0,0 +1,10 @@
+/home/sky/Projects/rustlings/target/debug/deps/libinotify-b62f775a32c43709.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/events.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/fd_guard.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/inotify.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/util.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/watches.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/inotify-b62f775a32c43709.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/events.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/fd_guard.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/inotify.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/util.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/watches.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/events.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/fd_guard.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/inotify.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/util.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/inotify-0.9.6/src/watches.rs:
