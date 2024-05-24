fe289251cd18eaea1ce94ea11a9656084a3e6d70
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/assert_cmd-ae5d3bc602deae08/build_script_build-ae5d3bc602deae08.d - Added :

---
+++ target/debug/build/assert_cmd-ae5d3bc602deae08/build_script_build-ae5d3bc602deae08.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/assert_cmd-ae5d3bc602deae08/build_script_build-ae5d3bc602deae08: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/assert_cmd-2.0.14/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/assert_cmd-ae5d3bc602deae08/build_script_build-ae5d3bc602deae08.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/assert_cmd-2.0.14/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/assert_cmd-2.0.14/build.rs:
