ad905dadc11b2bb9482834b1a4bc66ade3667b78
SKY <sky012877@gmail.com>
2024-05-24 13:02:05 -0700

    target/debug/deps/unicode_ident-fddcf84acc64a46f.d - Added :

---
+++ target/debug/deps/unicode_ident-fddcf84acc64a46f.d
@@ -0,0 +1,8 @@
+/home/sky/Projects/rustlings/target/debug/deps/libunicode_ident-fddcf84acc64a46f.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/libunicode_ident-fddcf84acc64a46f.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/unicode_ident-fddcf84acc64a46f.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/unicode-ident-1.0.12/src/tables.rs:
