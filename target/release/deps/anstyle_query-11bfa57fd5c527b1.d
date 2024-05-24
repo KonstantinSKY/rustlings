b46af150ee586cc54baf2e4f8c2e15ad43340a5d
SKY <sky012877@gmail.com>
2024-05-24 12:52:19 -0700

    target/release/deps/anstyle_query-11bfa57fd5c527b1.d - Added :

---
+++ target/release/deps/anstyle_query-11bfa57fd5c527b1.d
@@ -0,0 +1,8 @@
+/home/sky/Work/Projects/rustlings/target/release/deps/libanstyle_query-11bfa57fd5c527b1.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/windows.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/libanstyle_query-11bfa57fd5c527b1.rlib: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/windows.rs
+
+/home/sky/Work/Projects/rustlings/target/release/deps/anstyle_query-11bfa57fd5c527b1.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/lib.rs /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/windows.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/lib.rs:
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/anstyle-query-1.0.3/src/windows.rs:
