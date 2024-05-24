db8198272a3df991ce7d28ebe9c8cf9fba0fbb85
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/rustix-71a8e89edc848c47/build_script_build-71a8e89edc848c47.d - Added :

---
+++ target/debug/build/rustix-71a8e89edc848c47/build_script_build-71a8e89edc848c47.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/rustix-71a8e89edc848c47/build_script_build-71a8e89edc848c47: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustix-0.38.32/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/rustix-71a8e89edc848c47/build_script_build-71a8e89edc848c47.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustix-0.38.32/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/rustix-0.38.32/build.rs:
