d64885f6e95db335c5b971a0c3373590ab1d7234
SKY <sky012877@gmail.com>
2024-05-24 13:02:04 -0700

    target/debug/deps/bitflags-e131513f7bca03c0.d - Added :

---
+++ target/debug/deps/bitflags-e131513f7bca03c0.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/deps/libbitflags-e131513f7bca03c0.rmeta: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
+
+/home/sky/Projects/rustlings/target/debug/deps/bitflags-e131513f7bca03c0.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/bitflags-1.3.2/src/lib.rs:
