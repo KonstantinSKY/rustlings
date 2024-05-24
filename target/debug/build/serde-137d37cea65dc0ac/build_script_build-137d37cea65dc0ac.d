0fa2f413409bbd7853edb359d6392426048445b1
SKY <sky012877@gmail.com>
2024-05-24 13:02:03 -0700

    target/debug/build/serde-137d37cea65dc0ac/build_script_build-137d37cea65dc0ac.d - Added :

---
+++ target/debug/build/serde-137d37cea65dc0ac/build_script_build-137d37cea65dc0ac.d
@@ -0,0 +1,5 @@
+/home/sky/Projects/rustlings/target/debug/build/serde-137d37cea65dc0ac/build_script_build-137d37cea65dc0ac: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.198/build.rs
+
+/home/sky/Projects/rustlings/target/debug/build/serde-137d37cea65dc0ac/build_script_build-137d37cea65dc0ac.d: /home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.198/build.rs
+
+/home/sky/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.198/build.rs:
